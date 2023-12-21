e025035c01db5362d84250a238df7b3c4f57ca74
nikolasaugusto <101151419+nikolasaugusto@users.noreply.github.com>
2023-12-21 00:49:56 -0300

    ADD js, json, scss do slick

---
+++ src/js/SlickFragmento.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".slick-carousel").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+    });
+  });
